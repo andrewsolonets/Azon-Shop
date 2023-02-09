5003346365b17612de4fd692fc96796f9bea8eda
Andrew Solonets <andrewsolonets@gmail.com>
2023-02-09 10:01:35 +0000

    caching 3

--- next.config.mjs
+++ next.config.mjs
@@ -13,20 +13,7 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
-  async headers() {
-    return [
-      {
-        source: "/:all*(svg|jpg|png)",
-        locale: false,
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=9999999999, must-revalidate",
-          },
-        ],
-      },
-    ];
-  },
+
   images: {
     domains: [
       "shorturl.at",
